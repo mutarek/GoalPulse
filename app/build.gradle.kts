f3ebbc5c1ce6b0a0777598c89d865fcc7419f197
M Tarek <mohiuddintarek2017@gmail.com>
2026-05-20 13:18:49 +0600

    Add IDE project files and bump app version
    Add IntelliJ/Android Studio project configuration (.idea/*) including Gradle settings, run configurations, inspection profile, compiler and VCS mappings. Also update app/build.gradle.kts to bump versionCode to 2 and versionName to 1.0.1.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,8 @@ android {
         applicationId = "com.prosolution.goalpulse"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
